98e780c16243a0698ca1ad1bdb208a2fc1d152b5
Kamil <wldoekpl@gmail.com>
2026-02-01 12:35:18 +0100

    Accepted solution for InsertDeleteGetRandom

--- InsertDeleteGetRandom/insertDeleteGetRandom.cpp
+++ InsertDeleteGetRandom/insertDeleteGetRandom.cpp
@@ -1,4 +1,4 @@
-    // Link to problem
+// Link to problem
 // https://leetcode.com/problems/insert-delete-getrandom-o1/description/?envType=study-plan-v2&envId=top-interview-150
 //
 
@@ -6,6 +6,8 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <string>
+#include <iostream>
 
 class RandomizedSet {
 public:
@@ -19,7 +21,7 @@ public:
     }
     
     bool insert(int val) {
-        if (auto i = indexs.find(val); i != indexs.cend())
+        if (auto i = indexs.find(val); i == indexs.cend())
         {
             items.push_back(val);
             indexs[val] = items.size() - 1;
@@ -31,7 +33,9 @@ public:
     bool remove(int val) {
         if (auto i = indexs.find(val); i != indexs.cend())
         {
-            items[indexs[val]] = items[items.size() - 1];
+            int n = items.size();
+            items[indexs[val]] = items[n - 1];
+            indexs[items[n - 1]] = indexs[val];
             items.pop_back();
             indexs.erase(val);
             return true;
@@ -40,12 +44,47 @@ public:
     }
     
     int getRandom() {
-        const int random_value = std::rand() % (items.size() + 1);
+        const int random_value = std::rand() % (items.size());
         return items[random_value];
     }
 };
 
 int main()
 {
+    std::vector<std::string> operations = {"RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"};
+    std::vector<std::vector<int>> input = {{}, {1}, {2}, {2}, {}, {1}, {2}, {}};
+
+    RandomizedSet rs;
+    for (int i = 0; i < operations.size(); i++)
+    {
+        if (operations[i] == "RandomizedSet")
+        {
+            rs = RandomizedSet();
+            std::cout << "null" << std::endl;
+        }
+        else if (operations[i] == "insert")
+        {
+            for (auto& val: input[i])
+            {
+                std::cout << rs.insert(val) << std::endl;
+            }
+        }
+        else if (operations[i] == "remove")
+        {
+            for (auto& val: input[i])
+            {
+                std::cout << rs.remove(val) << std::endl;
+            }
+        }
+        else if (operations[i] == "getRandom")
+        {
+            std::cout << rs.getRandom() << std::endl;
+        }
+        else
+        {
+            std::cout << "unknown operation" << std::endl;
+        }
+    }
+
     return 0;
 }
